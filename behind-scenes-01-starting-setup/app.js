f03bafecc8c17d33b676b7aa7efad7d538b91a9e
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-02-01 17:30:07 -0500

    lifecycle vue

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+  beforeCreate() {
+    console.log("BeforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  }
+});
+
+app.mount('#app');
--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,20 +8,50 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null,
+            logMessages: []
         };
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: "0%"}
+            }
             return {width: this.monsterHealth + '%'}; 
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: "0%"};
+            }
             return {width: this.playerHealth + '%'}; 
         },
         mayUseSpecialAttack() {
-            return this.currentRound % 5 !== 0;
+            return this.currentRound % 3 !== 0;
         }
 
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                // a draw
+                this.winner = 'draw';
+            }
+            else if (value <= 0) {
+                // player dies
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                // a draw
+                this.winner = 'draw';
+            }
+            else if (value <= 0) {
+                // monster dies
+                this.winner = 'player';
+            }
+        }
     },
     methods: {
         attackMonster() {
@@ -30,11 +60,15 @@ const app = Vue.createApp({
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
 
+            this.addLogMessage('player', 'attack', attackValue);
+
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+
+            this.addLogMessage('monster', 'attack', attackValue);
         },
         specialAttackMonster() {
             this.currentRound += 1;
@@ -42,7 +76,43 @@ const app = Vue.createApp({
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
 
+            this.addLogMessage('player', 'special-attack', attackValue);
+
             this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound += 1;
+
+            const healthValue = getRandomValue(8, 20);
+            if (this.playerHealth + healthValue > 100) {
+                this.playerHealth = 100;
+            }
+            else {
+                this.playerHealth += healthValue;
+            }
+
+            this.addLogMessage('player', 'heal', healthValue);
+
+            this.attackPlayer();
+        },
+        restart() {
+            this.playerHealth = 100,
+            this.monsterHealth = 100,
+            this.currentRound = 0;
+            this.winner = null;
+            this.logMessages = [];
+        },
+        surrender() {
+            this.winner = 'monster';
+
+            // this.addLogMessage('player', 'surrender', '');
+        },
+        addLogMessage(who, what, value) {
+            this.logMessages.unshift({
+                actionBy:who,
+                actionType:what,
+                actionValue:value
+            });
         }
     }
 });
