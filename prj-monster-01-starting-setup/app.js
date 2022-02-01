38259bf49b5248c7580eb16da61b909083e7016c
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-01-31 19:18:06 -0500

    monster 2

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         };
     },
     computed: {
@@ -16,11 +17,16 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return {width: this.playerHealth + '%'}; 
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 5 !== 0;
         }
 
     },
     methods: {
         attackMonster() {
+            this.currentRound += 1;
+
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
 
@@ -29,6 +35,14 @@ const app = Vue.createApp({
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound += 1;
+
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+
+            this.attackPlayer();
         }
     }
 });
