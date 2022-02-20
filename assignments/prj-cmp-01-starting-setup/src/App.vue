779bb8e943e76992f179ea9a7ac7b20fc48c0c8c
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-02-20 17:20:33 -0500

    start of project 2

---
+++ assignments/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<the-header title="Remember Me">  </the-header>
+  <stored-resources
+    :resources="storedResources"
+  ></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue'
+
+export default {
+  components: {
+    StoredResources,
+    TheHeader 
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The offical Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to goggle...',
+          link: 'https://google.org'
+        }
+      ]
+    }
+  },
+  
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}  
+
+
+</style>
