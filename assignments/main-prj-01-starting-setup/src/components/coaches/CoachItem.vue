22943f12d02625d423b6091043ed2b0ce7d764e1
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-03-11 17:49:54 -0500

    vue class through coach

---
+++ assignments/main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ rate }}/hour</h4>
+    <div>
+      <base-badge 
+        v-for="area in areas"
+        :key="area"
+        :type="area"
+        :title="area"
+        >{{ area }}</base-badge>
+    </div>
+    <div class="actions">
+      <base-button link :to="coachContactLink" mode="outline">
+        Contact
+      </base-button>
+      <base-button link :to="coachDetailLink">
+        View Details
+      </base-button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    'id', 'firstName', 'lastName', 'rate', 'areas'
+  ],
+  computed: {
+    fullName() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    coachContactLink() {
+      // return '/coaches/' + this.id + '/contact'; - this is one way to do it but may be too much hard coding
+       return this.$route.path + '/' + this.id + '/contact'; 
+    },
+    coachDetailLink() {
+      // return '/coaches/' + this.id;    ditto from above
+      return this.$route.path + '/' + this.id; 
+    }
+  }
+}
+</script>
+
+<style>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- assignments/main-prj-01-starting-setup/src/store/index.js
+++ assignments/main-prj-01-starting-setup/src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'vuex';
 
+import coachesModule from './modules/coaches/index.js';
+
 const store = createStore({
-  
+  modules: {
+    coaches: coachesModule
+  }
 });
 
-export default staore;
+export default store;
